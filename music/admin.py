34ed8666c101ec59e4111d21417ef7fd5e8358de
Miguel Cantillana <miguel@ewok.cl>
2016-07-15 18:21:50 -0400

    termino de sistema con pauta para examen

--- music/admin.py
+++ music/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from music.models import Artista
+from music.models import Artista, Album, Track
 
 
 class ArtistaAdmin(admin.ModelAdmin):
@@ -7,4 +7,19 @@ class ArtistaAdmin(admin.ModelAdmin):
     # list_filter = ('owner','active')
     # list_search = ('name',)
 
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('nombre','ano','orden','artista','created_at','updated_at')
+    list_filter = ('artista','ano')
+    list_search = ('nombre',)
+
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('nombre','duracion_min','duracion_seg','numero','album','created_at','updated_at')
+    list_filter = ('album',)
+    list_search = ('nombre',)
+
+
 admin.site.register(Artista, ArtistaAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Track, TrackAdmin)
